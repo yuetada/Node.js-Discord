7785e91fc338594a985e50dc618ab2969529553c
yuetada <asdfzxc900@gmail.com>
2023-11-09 13:25:13 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,13 @@
+// Require the necessary discord.js classes (從discord.js套件裡面拿出前面3個來作使用)
+import { Client, Events, GatewayIntentBits } from 'discord.js'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.login(process.env.TOKEN);
